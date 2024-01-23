7225da704e250db4cb18068bbdcf78d66a9f0114
Skalman <skalman@riseup.net>
2024-01-23 18:20:24 +0100

    commit crate1 and crate2

---
+++ crate1/src/lib.rs
@@ -0,0 +1,18 @@
+pub trait Trait1 {
+
+}
+
+pub struct Struct1;
+
+impl Trait1 for Struct1{}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate2::Enum1;    
+
+    #[test]
+    fn it_works() {
+        let my_enum : Enum1<Struct1>;
+    }
+}
---
+++ crate2/src/lib.rs
@@ -0,0 +1,9 @@
+use crate1::{Struct1, Trait1};
+use std::marker::PhantomData;
+
+pub enum Enum1<GenericStruct> where GenericStruct : Trait1 {
+    Val1,
+    _Marker(PhantomData<GenericStruct>),
+}
+
+
